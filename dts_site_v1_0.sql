b885c3c10c01c702148dd3df33b2b61b0589b688
ptearle <pete.tearle@globalspecimensolutions.com>
2016-08-01 20:51:00 -0400

    Added dts_specimen_inventory_v1_0 and dts_site_v1_0 table scripts.

---
+++ dts_site_v1_0.sql
@@ -0,0 +1,25 @@
+CREATE TABLE dts_site_v1_0 (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  study_protocol_id varchar(50) DEFAULT NULL,
+  site_number varchar(50) DEFAULT NULL,
+  site_name varchar(255) DEFAULT NULL,
+  site_address varchar(255) DEFAULT NULL,
+  site_city varchar(50) DEFAULT NULL,
+  site_state varchar(50) DEFAULT NULL,
+  site_country varchar(50) DEFAULT NULL,
+  site_postal_code varchar(50) DEFAULT NULL,
+  site_phone varchar(50) DEFAULT NULL,
+  site_site_fax varchar(50) DEFAULT NULL,
+  site_FPFV date DEFAULT NULL,
+  site_LPLV date DEFAULT NULL,
+  planned_enrollment int(11) DEFAULT NULL,
+  site_PI varchar(50) DEFAULT NULL,
+  site_PI_email varchar(50) DEFAULT NULL,
+  site_coordinator varchar(50) DEFAULT NULL,
+  site_coordinator_email varchar(50) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB
+AUTO_INCREMENT = 1
+CHARACTER SET utf8
+COLLATE utf8_unicode_ci;
