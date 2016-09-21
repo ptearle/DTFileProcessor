a8fb4c31a1d8641d29dc0c9d02aa7c713f511a08
ptearle <pete.tearle@globalspecimensolutions.com>
2016-09-21 11:56:25 -0400

    Corrected test root path

--- dt_env.rb
+++ dt_env.rb
@@ -5,7 +5,7 @@ class DT_env
       @root_dir = (env == 'PROD') ? 'C:\SFTP_PROD' : 'C:\SFTP_TEST'
       @dir_separator = '\\'
     else
-      @root_dir = (env == 'PROD') ? '/home/vendors/gss/prod' : '/home/vendor/gss/test'
+      @root_dir = (env == 'PROD') ? '/home/vendors/gss/prod' : '/home/vendors/gss/test'
       @dir_separator = '/'
     end
   end
